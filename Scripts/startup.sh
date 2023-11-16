95575b14d7363efe810cdbd6a861ac265b3dd43b
vistrate <ioan.vlad.istrate02@gmail.com>
2023-11-16 15:17:28 +0200

    Implement docker syncing local files to static volume + update gitignore

---
+++ Scripts/startup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Waiting for app to start on localhost:3000
+
+exec npm start &
+
+echo "Waiting for app to start on localhost:3000..."
+while ! nc -z localhost 3000; do
+  sleep 1
+done
+
+echo "App started, syncing files..."
+# Syncing files
+while true; do
+  rsync -a --delete --info=NAME --quiet /local_build/ /volume_build
+  sleep 3
+done
--- Writerside/topics/RedirectButton.md
+++
@@ -1,4 +0,0 @@
-# RedirectButton
-It's a functional button that redirects the user to a specified path.  
-The syntax is ```<RedirectButton path='/path' text='Button text'/>```  
-Please note that path and text are mandatory for the button to function properly.
