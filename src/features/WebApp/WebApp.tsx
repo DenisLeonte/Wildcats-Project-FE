b5a30075deb13e1de29819a5634c360ab3e877d7
LeonteDenis <denis.leonte123@gmail.com>
2023-11-17 20:16:47 +0200

    Begun menu dev

--- src/features/WebApp/WebApp.tsx
+++ src/features/WebApp/WebApp.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Popup from 'reactjs-popup'
+import CityCostOfLivingPopup from '../../components/COFDetailsCityPopup/COFDetails';
 
 const WebApp: React.FC = () => {
     return (
         <div>
             WebApp component
+            <Popup trigger={<button>Test</button>} modal>
+                <CityCostOfLivingPopup></CityCostOfLivingPopup>
+            </Popup>
         </div>
     );
 }
