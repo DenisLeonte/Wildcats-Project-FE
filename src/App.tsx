57189ee4e514f23be73e9f11b6bd5491ac1ffecc
LeonteDenis <denis.leonte123@gmail.com>
2023-11-22 21:15:34 +0200

    Full page set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
-import WebApp from './features/WebApp/WebApp';
-import './styles/App.css';
-import Home from './features/Home/Home';
+import React from 'react';
+import ReactFullpage from '@fullpage/react-fullpage';
 
+function App(){
+  const anchors = ["page1","page2","page3"];
+  const creds = {
+    enabled:true,
+    label:"made possible by fullPage.js",
+    position:"right"
+  }
+  return (
+    <div className="app">
+      <ReactFullpage
+        anchors={anchors}
+        navigation
+        navigationTooltips={anchors}
+        licenseKey={process.env.REACT_APP_FULLPAGE_LK}
+        scrollingSpeed={1000}
+        scrollOverflow={true}
+        // JUST FOR DEV I WILL FIX ASAP
+        //@ts-ignore
+        credits={creds}
+        render={({state}) =>{
+          return(
+            <ReactFullpage.Wrapper>
+              <div className="section background_image1">
 
-function App() {
-  return(
-  <div>
-      <Router>
-        <Switch>
-          <Route path={"/webapp"} component={WebApp}/>
-          <Route path={"/landing"} component={Home}/>
-        </Switch>
-      </Router>
-  </div>
-  )
+              </div>
+              <div className="section"> Page 2</div>
+            </ReactFullpage.Wrapper>
+          )
+        }}
+        ></ReactFullpage>
+    </div>
+  );
 }
+
 export default App;
