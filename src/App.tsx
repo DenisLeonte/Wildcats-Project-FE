e4f984e1984fccea826ca0050c122a68cf7ac2fa
LeonteDenis <denis.leonte123@gmail.com>
2023-12-05 12:53:43 +0200

    Added routing support

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import HomePage from "./components/features/HomePage/HomePage"
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App(){
   return(
-    <HomePage/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
